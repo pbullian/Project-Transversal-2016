2fd77fb8de95770f51beecc0ebc9c47d5ec35553
pbullian <Pablo@Pauls-Warmachine.local>
2016-03-31 00:57:59 +0200

    added responses

--- FO-M2/serializer.c
+++ FO-M2/serializer.c
@@ -13,6 +13,10 @@
 #include <stdio.h>   
 #include <Structure_Serializer.c>
 #include <string.h>
+#include <c8051f020.h>  										// SFR declarations
+#include <LIB_BSE_Config_Globale.h>
+#include <LIB_BSE_GPIO.h>
+#include <LIB_BSE_UART.h>
 
 //recibo la estructura y traduzco los comandos.
 
@@ -20,25 +24,32 @@
 //Send_String();
 
 
-void serializer_traductor(struct COMMANDES_SERIALIZER s){
+struct talk_serializer(struct COMMANDES_SERIALIZER s){
     char buf1[15];
     char command[25];
+    struct INFORMATIONS_SERIALIZER r;
 
     //Commande_non, Reset, Getenc_1, Getenc_2, Getenc_1_2, Clrenc_1, Clrenc_2, Clrenc_1_2, mogo_1, mogo_2, mogo_1_2, Vpid_set, Vpid_read,digo_1, digo_2, digo_1_2, Dpid_set, Dpid_read, Rpid_Stinger, Pids, Stop, Vel, Restore
     switch(s.Commande) {
         case 'Reset' :
-            Send_Serializer('reset');
+
+            Send_Serializer('reset', 1);
             break;
 
             //The getenc command returns the values of the encoder count (channel B) for the specified encoder Id(s). NOTE: The encoder counts for channel A are used for internal VPID and DPID algorithms.
         case 'Getenc_1' :
             Send_Serializer('getenc 1');
+            //listen to response
+            r= receive_traduite(serializer_response(),1);
             break;
         case 'Getenc_2' :
             Send_Serializer('getenc 2');
+            r= receive_traduite(serializer_response(),2);
             break;
         case 'Getenc_1_2' :
             Send_Serializer('getenc 1 2');
+            r= receive_traduite(serializer_response(),3);
+
             break;
 
             //clrenc encoderId [encoderId]
@@ -100,6 +111,8 @@ You will have to determine which direction is positive for your motors, and conn
             break;
         case 'Vpid_read' :
             Send_Serializer('vpid');
+            r= receive_traduite(serializer_response(),4);
+
             break;
 
 
@@ -167,6 +180,8 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
             break;
         case 'Dpid_read' :
             Send_Serializer('dpid');
+            r=  receive_traduite(serializer_response(),5);
+
             break;
             /*The rpid command sets the default PID params known to work with either the Stinger or Traxster Robotic Kits in the firmware. This makes it quick and easy to set up the PID params for both robots.*/
 
@@ -178,6 +193,8 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
 */
         case 'Pids' :
             Send_Serializer('pids');
+            r=  receive_traduite(serializer_response(),6);
+
             break;
 
             /*The stop command immediately stops motor 1 and 2 connected to the motor ports. This will override any pwm, mogo, or digo commands which are currently active.*/
@@ -188,6 +205,8 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
 velocity returned is based on the PIDL parameter configuration.*/
         case 'Vel' :
             Send_Serializer('vel');
+            r=  receive_traduite(serializer_response(),7);
+
             break;
             /*Restores the factory default settings, and resets the board. NOTE: This will erase any configurations you have saved to EEPROM, including VPID, DPID, and baud rate settings.*/
         case 'Restore' :
@@ -195,4 +214,101 @@ velocity returned is based on the PIDL parameter configuration.*/
             break;
 
     }
+    return r;
+}
 
+
+
+void Send_Serializer(char *char_ptr){
+    xdata char car_recu;
+    car_recu=getchar();   										//Reception du caractere
+    if(car_recu=='>')					//Controle de la réponse aux caractères 'r' et 'R' uniquement
+    {
+        Send_String(*char_ptr);
+        Send_String('\r');
+        //parte de M1 que va a recevoir les strings complet
+    }
+}
+
+//reply from the serializer
+char *serializer_response(){
+
+    //TODO: avec F0-M1
+}
+
+
+//receive a string (response) with the response, we cant utilize switch with a chain
+struct receive_traduite(char *response, int option){
+    char buf1[15];
+    char buf2[15];
+    char * pch;
+    struct INFORMATIONS_SERIALIZER r;
+
+    //Reponse_non, Rep_getenc, Rep_vpid, Rep_dpid, Rep_pids, Rep_vel
+    switch(response) {
+
+            //The getenc command returns the values of the encoder count (channel B) for the specified encoder Id(s). NOTE: The encoder counts for channel A are used for internal VPID and DPID algorithms.
+        case 1 :
+            r.Etat_Response=Rep_getenc;
+            r.Read_Val_enc_1 = sscanf(response, "%d", &i);
+            break;
+        case 2 :
+            r.Etat_Response=Rep_getenc;
+            r.Read_Val_enc_2 = sscanf(response, "%d", &i);
+            break;
+        case 3 :
+            r.Etat_Response=Rep_getenc;
+            pch = strtok (str," ");
+            r.Read_Val_enc_1 = pch;
+            r.Read_Val_enc_2 = pch;
+            break;
+
+
+            /*The vpid command gets/sets the PIDL (Proportional, Integral, Derivative, and Loop) parameters for the Velocity PID control on the SerializerTM. If the PIDL parameters are absent, the PIDL values are returned. Otherwise the PIDL parameters are parsed, and saved (in eeprom). */
+
+
+        case 4 :
+            r.Etat_Response=Rep_vpid;
+            pch = strtok (str," ");
+            r.Read_P = pch;
+            r.Read_I = pch;
+            r.Read_D = pch;
+            r.Read_L_A = pch;
+            break;
+
+
+            /*
+           The dpid command gets/sets the PIDA (Proportional, Integral, Derivative, and Acceleration) parameters for the distance PID control on the SerializerTM. If the PIDA parameters are absent, the PIDA values are returned.
+           */
+
+
+
+        case 5 :
+            r.Etat_Response=Rep_dpid;
+            pch = strtok (str," ");
+            r.Read_P = pch;
+            r.Read_I = pch;
+            r.Read_D = pch;
+            r.Read_L_A = pch;
+            break;
+
+            /*Once a digo command is issued, an internal state variable within the firmware is set to ‘1’, and it stays in that state until the algorithm has completed. Upon completion, the state is set to ‘0’. The ‘pids’ command simply returns the value of the internal variable to determine if the algorithms is currently busy, or if it has finished, thus allowing subsequent digo commands to be issued w/o clobbering previous ones.
+*/
+        case 6 :
+            r.Etat_Response=Rep_pids;
+            pch = strtok (str," ");
+            r.Read_Pids = sscanf(response, "%d", &i);
+            break;
+
+            /*The vel command returns the left and right wheel velocities. The
+velocity returned is based on the PIDL parameter configuration.*/
+        case 7 :
+            r.Etat_Response=Rep_vel;
+            pch = strtok (str," ");
+            r.Read_Vitesse_mot1 = pch;
+            r.Read_Vitesse_mot2 = pch;
+            break;
+
+    }
+    return r;
+}
