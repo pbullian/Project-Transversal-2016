e10555c713e227f3a5fe8fd1812a152a94f5867e
pbullian <Pablo@Pauls-Warmachine.local>
2016-04-06 23:33:15 +0200

    version 4 sans recevoir commands de serial

--- FO-M2/BSE_Main.c
+++ FO-M2/BSE_Main.c
@@ -11,71 +11,72 @@
 // Includes
 //------------------------------------------------------------------------------------
 //#include <stdio.h>   
+
 #include <c8051f020.h>  										// SFR declarations
 #include <LIB_BSE_Config_Globale.h>
 #include <LIB_BSE_GPIO.h>
 #include <LIB_BSE_UART.h>
+#include <Structure_Serializer.c>
 
-//----------------------------------------------------------------------------
-// Reponse
-//----------------------------------------------------------------------------
-void Reponse(void)
-{
-	xdata char car_recu;
-	car_recu=getchar();   										//Reception du caractere
-	if(car_recu=='R'||car_recu=='r')					//Controle de la r»ponse aux caractÀres 'r' et 'R' uniquement
-	{
-		Send_String("Test reussi");
-		//Send_String(&nbr_colis);
-		Send_String("\0");
-	}
-}
-void main (void) {
- 
+#include <Structures.h>
+
+//
+//#include <stdio.h>
+//    /* card structure definition */
+//    struct card
+//    {
+//      int face; // define pointer face
+//    };// end structure card
+//
+//    typedef struct card Card ;
+//
+//    /* prototype */
+//    void passByReference(Card *c) ;
+//
+//    int main(void)
+//    {
+//    Card c ;
+//    c.face = 1 ;
+//    Card *cptr = &c ; // pointer to Card c
+//
+//    printf("The value of c before function passing = %d\n", c.face);
+//    printf("The value of cptr before function = %d\n",cptr->face);
+//
+//    passByReference(cptr);
+//
+//    printf("The value of c after function passing = %d\n", c.face);
+//
+//        return 0 ; // successfully ran program
+//    }
+//
+//
+//    void passByReference(Card *c)
+//    {
+//        c->face = 4;
+//    }
+//
+
+
+typedef struct COMMANDES_SERIALIZER Struct_R ;
+typedef struct INFORMATIONS_SERIALIZER Struct_E;
+
+
+
+
+void serializer (Struct_R *s,Struct_E *r) {
+
+ Struct_R *cpts = &s ;
+ Struct_E *cptr = &r ;
+
+    //this should be done in another file
    Init_Device();  													// Initialisation du microcontrŸleur
 	 Port_IO_Init();
 	 CFG_UART1();
+    //
+
+
+talk_serializer(cpts,cptr);
+
 	 
-//------------------------------------------------------------------------------------
-// TEST
-//------------------------------------------------------------------------------------
-//Send_String("cfg baud 3\r");
 
-//Send_String("vpid 10:0:0:700\r");
-	//Send_String("mogo 1:45 2:45\r");
-		//Send_String("blink 1:50 2:100\r");
-
-	while(1)
-	{
-	xdata char car_recu;
-
-		//putchar('\n',80);
-		car_recu=getchar();
-		if(car_recu=='>')
-				{
-		Send_String("mogo 1:45 2:45\r\n\r");
-		car_recu='p';
-				//	Send_String("blink 1:50 2:50\r\n");
-		}else if (car_recu=='s')				{
-		Send_String("stop\r\n");
-		car_recu='p';
-			}
-		//Send_String("\r\n");
-	//	car_recu=getchar();      //Reception du caractere
-		//putchar(car_recu,80);
-		//putchar('\r',80);
-
-		//car_recu='e';
-		//Send_String("\n\r");
-
-		/*if(car_recu=="R"||car_recu=='r')					//Controle de la r»ponse aux caractÀres 'r' et 'R' uniquement
-		{
-			//Send_String("Test reussi\n\r");
-			Send_String("mogo 1:45 2:45\r");
-			Send_String("\r");
-		}*/
-		//Reponse();
-	  //test_getchar_putchar_sendstring();
-
-	}
 }
--- FO-M2/serializer.c
+++ FO-M2/serializer.c
@@ -11,39 +11,45 @@
 // Includes
 //------------------------------------------------------------------------------------
 #include <stdio.h>   
-#include <Structure_Serializer.c>
+#include <Structures.h>
 #include <string.h>
 #include <c8051f020.h>  										// SFR declarations
-#include <LIB_BSE_Config_Globale.h>
-#include <LIB_BSE_GPIO.h>
+//#include <LIB_BSE_Config_Globale.h>
+//#include <LIB_BSE_GPIO.h>
 #include <LIB_BSE_UART.h>
 
+typedef struct COMMANDES_SERIALIZER Struct_R ;
+typedef struct INFORMATIONS_SERIALIZER Struct_E;
 
-struct talk_serializer(struct COMMANDES_SERIALIZER s){
+
+void talk_serializer(Struct_R  *s, Struct_E *r){
     char buf1[15];
     char command[25];
-    struct INFORMATIONS_SERIALIZER r;
+
+     Struct_E *cptr = &r ;
+
+
 
     //Commande_non, Reset, Getenc_1, Getenc_2, Getenc_1_2, Clrenc_1, Clrenc_2, Clrenc_1_2, mogo_1, mogo_2, mogo_1_2, Vpid_set, Vpid_read,digo_1, digo_2, digo_1_2, Dpid_set, Dpid_read, Rpid_Stinger, Pids, Stop, Vel, Restore
-    switch(s.Commande) {
+    switch(s->Commande) {
         case 'Reset' :
 
-            Send_Serializer('reset', 1);
+            Send_Serializer('reset');
             break;
 
             //The getenc command returns the values of the encoder count (channel B) for the specified encoder Id(s). NOTE: The encoder counts for channel A are used for internal VPID and DPID algorithms.
         case 'Getenc_1' :
             Send_Serializer('getenc 1');
             //listen to response
-            r= receive_traduite(serializer_response(),1);
+            receive_traduite(serializer_response(),1,cptr);
             break;
         case 'Getenc_2' :
             Send_Serializer('getenc 2');
-            r= receive_traduite(serializer_response(),2);
+            receive_traduite(serializer_response(),2,cptr);
             break;
         case 'Getenc_1_2' :
             Send_Serializer('getenc 1 2');
-            r= receive_traduite(serializer_response(),3);
+            receive_traduite(serializer_response(),3,cptr);
 
             break;
 
@@ -58,29 +64,29 @@ struct talk_serializer(struct COMMANDES_SERIALIZER s){
         case 'Clrenc_1_2' :
             Send_Serializer('clrenc 1 2');
             break;
-            /*The mogo command sets motor speed using one or more complex parameters containing a <motorId:spd> value pair.
+            /*The mogo command sets motor speed using one or more complex parameters containing a <motorId:spd> value pair->
 The motorId can be either 1 or 2, which corresponds to the Motor Terminal port.
 The vel value specifies the motor velocity, and it’s range depends on your VPID settings. See the VPID parameters section below to determine your MAX velocity. A positive value rotates the motors in one direction, which a negative value rotates the motors in the opposite direction.
 You will have to determine which direction is positive for your motors, and connect the motors wires to the terminals on the Serializer board in the appropriate configuration.*/
 
         case 'mogo_1' :
-            sprintf(buf1, "%d", s.Vitesse_Mot1);
+            sprintf(buf1, "%d", s->Vitesse_Mot1);
             strcpy(command, "mogo 1:");
             strcpy(command, buf1);
             Send_Serializer(command);
             break;
         case 'mogo_2' :
-            sprintf(buf1, "%d", s.Vitesse_Mot2);
+            sprintf(buf1, "%d", s->Vitesse_Mot2);
             strcpy(command, "mogo 2:");
             strcpy(command, buf1);
             Send_Serializer(command);
             break;
         case 'mogo_1_2' :
-            sprintf(buf1, "%d", s.Vitesse_Mot1);
+            sprintf(buf1, "%d", s->Vitesse_Mot1);
 
             strcpy(command, "mogo 1:");
             strcpy(command, buf1);
-            sprintf(buf1, "%d", s.Vitesse_Mot2);
+            sprintf(buf1, "%d", s->Vitesse_Mot2);
 
             strcpy(command, " 2:");
             strcpy(command, buf1);
@@ -90,23 +96,23 @@ You will have to determine which direction is positive for your motors, and conn
 
         case 'Vpid_set' :
             strcpy(command, "vpid ");
-            sprintf(buf1, "%d", s.Set_P);
+            sprintf(buf1, "%d", s->Set_P);
             strcpy(command, buf1);
             strcpy(command, ":");
-            sprintf(buf1, "%d", s.Set_I);
+            sprintf(buf1, "%d", s->Set_I);
             strcpy(command, buf1);
             strcpy(command, ":");
-            sprintf(buf1, "%d", s.Set_D);
+            sprintf(buf1, "%d", s->Set_D);
             strcpy(command, buf1);
             strcpy(command, ":");
-            sprintf(buf1, "%d", s.Set_L_A);
+            sprintf(buf1, "%d", s->Set_L_A);
             strcpy(command, buf1);
 
             Send_Serializer(command);
             break;
         case 'Vpid_read' :
             Send_Serializer('vpid');
-            r= receive_traduite(serializer_response(),4);
+            receive_traduite(serializer_response(),4,cptr);
 
             break;
 
@@ -120,34 +126,34 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
 
         case 'digo_1' :
             strcpy(command, "digo 1 : ");
-            sprintf(buf1, "%d", s.Ticks_mot1);
+            sprintf(buf1, "%d", s->Ticks_mot1);
             strcpy(command, buf1);
             strcpy(command, " : ");
-            sprintf(buf1, "%d", s.Vitesse_Mot1);
+            sprintf(buf1, "%d", s->Vitesse_Mot1);
             strcpy(command, buf1);
             Send_Serializer(command);
             break;
         case 'digo_2' :
             strcpy(command, "digo 2 : ");
-            sprintf(buf1, "%d", s.Ticks_mot2);
+            sprintf(buf1, "%d", s->Ticks_mot2);
             strcpy(command, buf1);
             strcpy(command, " : ");
-            sprintf(buf1, "%d", s.Vitesse_Mot2);
+            sprintf(buf1, "%d", s->Vitesse_Mot2);
             strcpy(command, buf1);
             Send_Serializer(command);
             break;
         case 'digo_1_2' :
             strcpy(command, "digo 1 : ");
-            sprintf(buf1, "%d", s.Ticks_mot1);
+            sprintf(buf1, "%d", s->Ticks_mot1);
             strcpy(command, buf1);
             strcpy(command, " : ");
-            sprintf(buf1, "%d", s.Vitesse_Mot1);
+            sprintf(buf1, "%d", s->Vitesse_Mot1);
             strcpy(command, buf1);
             strcpy(command, " 2 : ");
-            sprintf(buf1, "%d", s.Ticks_mot2);
+            sprintf(buf1, "%d", s->Ticks_mot2);
             strcpy(command, buf1);
             strcpy(command, " : ");
-            sprintf(buf1, "%d", s.Vitesse_Mot2);
+            sprintf(buf1, "%d", s->Vitesse_Mot2);
             strcpy(command, buf1);
             Send_Serializer(command);
             break;
@@ -159,23 +165,23 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
 
         case 'Dpid_set' :
             strcpy(command, "dpid ");
-            sprintf(buf1, "%d", s.Set_P);
+            sprintf(buf1, "%d", s->Set_P);
             strcpy(command, buf1);
             strcpy(command, ":");
-            sprintf(buf1, "%d", s.Set_I);
+            sprintf(buf1, "%d", s->Set_I);
             strcpy(command, buf1);
             strcpy(command, ":");
-            sprintf(buf1, "%d", s.Set_D);
+            sprintf(buf1, "%d", s->Set_D);
             strcpy(command, buf1);
             strcpy(command, ":");
-            sprintf(buf1, "%d", s.Set_L_A);
+            sprintf(buf1, "%d", s->Set_L_A);
             strcpy(command, buf1);
 
             Send_Serializer(command);
             break;
         case 'Dpid_read' :
             Send_Serializer('dpid');
-            r=  receive_traduite(serializer_response(),5);
+            receive_traduite(serializer_response(),5,cptr);
 
             break;
             /*The rpid command sets the default PID params known to work with either the Stinger or Traxster Robotic Kits in the firmware. This makes it quick and easy to set up the PID params for both robots.*/
@@ -188,7 +194,7 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
 */
         case 'Pids' :
             Send_Serializer('pids');
-            r=  receive_traduite(serializer_response(),6);
+            receive_traduite(serializer_response(),6,cptr);
 
             break;
 
@@ -200,7 +206,7 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
 velocity returned is based on the PIDL parameter configuration.*/
         case 'Vel' :
             Send_Serializer('vel');
-            r=  receive_traduite(serializer_response(),7);
+            receive_traduite(serializer_response(),7,cptr);
 
             break;
             /*Restores the factory default settings, and resets the board. NOTE: This will erase any configurations you have saved to EEPROM, including VPID, DPID, and baud rate settings.*/
@@ -209,18 +215,17 @@ velocity returned is based on the PIDL parameter configuration.*/
             break;
 
     }
-    return r;
+
 }
 
 
 
-void Send_Serializer(char *char_ptr){
-    xdata char car_recu;
-    car_recu=getchar();   										//Reception du caractere
-    if(car_recu=='>')					//Controle de la réponse aux caractères 'r' et 'R' uniquement
-    {
-        //TODO: avec F0-M1
-    }
+void Send_Serializer(const char* char_ptr){
+    char command[50];
+    strcpy(command, char_ptr);
+     strcpy(command, "\r\n\r");
+    Send_String(command);
+	return 0;
 }
 
 //reply from the serializer
@@ -231,29 +236,28 @@ char *serializer_response(){
 
 
 //receive a string (response) with the response, we can't utilize switch with an array of char
-struct receive_traduite(char *response, int option){
+void receive_traduite(char *response, int option, Struct_E *r){
     char buf1[15];
     char buf2[15];
     char * pch;
-    struct INFORMATIONS_SERIALIZER r;
 
     //Reponse_non, Rep_getenc, Rep_vpid, Rep_dpid, Rep_pids, Rep_vel
     switch(response) {
 
             //The getenc command returns the values of the encoder count (channel B) for the specified encoder Id(s). NOTE: The encoder counts for channel A are used for internal VPID and DPID algorithms.
         case 1 :
-            r.Etat_Response=Rep_getenc;
-            r.Read_Val_enc_1 = sscanf(response, "%d", &i);
+            r->Etat_Response=Rep_getenc;
+            r->Read_Val_enc_1 = sscanf(response, "%d", &i);
             break;
         case 2 :
-            r.Etat_Response=Rep_getenc;
-            r.Read_Val_enc_2 = sscanf(response, "%d", &i);
+            r->Etat_Response=Rep_getenc;
+            r->Read_Val_enc_2 = sscanf(response, "%d", &i);
             break;
         case 3 :
-            r.Etat_Response=Rep_getenc;
+            r->Etat_Response=Rep_getenc;
             pch = strtok (str," ");
-            r.Read_Val_enc_1 = pch;
-            r.Read_Val_enc_2 = pch;
+            r->Read_Val_enc_1 = pch;
+            r->Read_Val_enc_2 = pch;
             break;
 
 
@@ -261,12 +265,12 @@ struct receive_traduite(char *response, int option){
 
 
         case 4 :
-            r.Etat_Response=Rep_vpid;
+            r->Etat_Response=Rep_vpid;
             pch = strtok (str," ");
-            r.Read_P = pch;
-            r.Read_I = pch;
-            r.Read_D = pch;
-            r.Read_L_A = pch;
+            r->Read_P = pch;
+            r->Read_I = pch;
+            r->Read_D = pch;
+            r->Read_L_A = pch;
             break;
 
 
@@ -277,31 +281,31 @@ struct receive_traduite(char *response, int option){
 
 
         case 5 :
-            r.Etat_Response=Rep_dpid;
+            r->Etat_Response=Rep_dpid;
             pch = strtok (str," ");
-            r.Read_P = pch;
-            r.Read_I = pch;
-            r.Read_D = pch;
-            r.Read_L_A = pch;
+            r->Read_P = pch;
+            r->Read_I = pch;
+            r->Read_D = pch;
+            r->Read_L_A = pch;
             break;
 
             /*Once a digo command is issued, an internal state variable within the firmware is set to ‘1’, and it stays in that state until the algorithm has completed. Upon completion, the state is set to ‘0’. The ‘pids’ command simply returns the value of the internal variable to determine if the algorithms is currently busy, or if it has finished, thus allowing subsequent digo commands to be issued w/o clobbering previous ones.
 */
         case 6 :
-            r.Etat_Response=Rep_pids;
+            r->Etat_Response=Rep_pids;
             pch = strtok (str," ");
-            r.Read_Pids = sscanf(response, "%d", &i);
+            r->Read_Pids = sscanf(response, "%d", &i);
             break;
 
             /*The vel command returns the left and right wheel velocities. The
 velocity returned is based on the PIDL parameter configuration.*/
         case 7 :
-            r.Etat_Response=Rep_vel;
+            r->Etat_Response=Rep_vel;
             pch = strtok (str," ");
-            r.Read_Vitesse_mot1 = pch;
-            r.Read_Vitesse_mot2 = pch;
+            r->Read_Vitesse_mot1 = pch;
+            r->Read_Vitesse_mot2 = pch;
             break;
 
     }
-    return r;
+return 0;
 }
