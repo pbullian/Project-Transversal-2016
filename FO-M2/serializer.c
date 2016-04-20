739eadc297759298ab1d187babcb81dbfe082c1a
pbullian <Pablo@PaulsWarmachine.station>
2016-04-20 12:01:15 +0200

    implement ring buffer

--- FO-M2/serializer.c
+++ FO-M2/serializer.c
@@ -218,7 +218,7 @@ Distance specifies the distance (in encoder ticks ) which you want your robot to
 */
         case Pids :
             Send_Serializer("pids");
-            //          receive_traduite(serializer_response(),6,cptr);
+            receive_traduite(serializer_response(),6,cptr);
 
             break;
 
@@ -249,88 +249,89 @@ void Send_Serializer(const char* char_ptr){
 
     strcpy(command, char_ptr);
     strcat(command, "\r\n\r");
-    Send_String(command);
+    //Send_String(command);
+    serOutstring(command);
 }
 
 //reply from the serializer
-void serializer_response(){
-
-    //TODO: avec F0-M1
-
+char * serializer_response(){
+    char xdata response[50];
+    response=serInBuf();
+    return response;
 }
 
 
 ////receive a string (response) with the response, we can't utilize switch with an array of char
-//void receive_traduite(char *response, int option,struct INFORMATIONS_SERIALIZER *r){
-//    char buf1[15];
-//    char buf2[15];
-//    char * pch;
+void receive_traduite(char *response, int option,struct INFORMATIONS_SERIALIZER *r){
+    char buf1[15];
+    char buf2[15];
+    char * pch;
 
-//    //Reponse_non, Rep_getenc, Rep_vpid, Rep_dpid, Rep_pids, Rep_vel
-//    switch(response) {
+    //Reponse_non, Rep_getenc, Rep_vpid, Rep_dpid, Rep_pids, Rep_vel
+    switch(response) {
 
-//            //The getenc command returns the values of the encoder count (channel B) for the specified encoder Id(s). NOTE: The encoder counts for channel A are used for internal VPID and DPID algorithms.
-//        case 1 :
-//            r->Etat_Response=Rep_getenc;
-//            r->Read_Val_enc_1 = sscanf(response, "%d", &i);
-//            break;
-//        case 2 :
-//            r->Etat_Response=Rep_getenc;
-//            r->Read_Val_enc_2 = sscanf(response, "%d", &i);
-//            break;
-//        case 3 :
-//            r->Etat_Response=Rep_gestenc;
-//            pch = strtok (str," ");
-//            r->Read_Val_enc_1 = pch;
-//            r->Read_Val_enc_2 = pch;
-//            break;
-
-
-//            /*The vpid command gets/sets the PIDL (Proportional, Integral, Derivative, and Loop) parameters for the Velocity PID control on the SerializerTM. If the PIDL parameters are absent, the PIDL values are returned. Otherwise the PIDL parameters are parsed, and saved (in eeprom). */
+            //The getenc command returns the values of the encoder count (channel B) for the specified encoder Id(s). NOTE: The encoder counts for channel A are used for internal VPID and DPID algorithms.
+        case 1 :
+            r->Etat_Response=Rep_getenc;
+            r->Read_Val_enc_1 = sscanf(response, "%d", &i);
+            break;
+        case 2 :
+            r->Etat_Response=Rep_getenc;
+            r->Read_Val_enc_2 = sscanf(response, "%d", &i);
+            break;
+        case 3 :
+            r->Etat_Response=Rep_gestenc;
+            pch = strtok (str," ");
+            r->Read_Val_enc_1 = pch;
+            r->Read_Val_enc_2 = pch;
+            break;
 
 
-//        case 4 :
-//            r->Etat_Response=Rep_vpid;
-//            pch = strtok (str," ");
-//            r->Read_P = pch;
-//            r->Read_I = pch;
-//            r->Read_D = pch;
-//            r->Read_L_A = pch;
-//            break;
+            /*The vpid command gets/sets the PIDL (Proportional, Integral, Derivative, and Loop) parameters for the Velocity PID control on the SerializerTM. If the PIDL parameters are absent, the PIDL values are returned. Otherwise the PIDL parameters are parsed, and saved (in eeprom). */
 
 
-//            /*
-//           The dpid command gets/sets the PIDA (Proportional, Integral, Derivative, and Acceleration) parameters for the distance PID control on the SerializerTM. If the PIDA parameters are absent, the PIDA values are returned.
-//           */
+        case 4 :
+            r->Etat_Response=Rep_vpid;
+            pch = strtok (str," ");
+            r->Read_P = pch;
+            r->Read_I = pch;
+            r->Read_D = pch;
+            r->Read_L_A = pch;
+            break;
+
+
+            /*
+           The dpid command gets/sets the PIDA (Proportional, Integral, Derivative, and Acceleration) parameters for the distance PID control on the SerializerTM. If the PIDA parameters are absent, the PIDA values are returned.
+           */
 
 
 
-//        case 5 :
-//            r->Etat_Response=Rep_dpid;
-//            pch = strtok (str," ");
-//            r->Read_P = pch;
-//            r->Read_I = pch;
-//            r->Read_D = pch;
-//            r->Read_L_A = pch;
-//            break;
+        case 5 :
+            r->Etat_Response=Rep_dpid;
+            pch = strtok (str," ");
+            r->Read_P = pch;
+            r->Read_I = pch;
+            r->Read_D = pch;
+            r->Read_L_A = pch;
+            break;
 
-//            /*Once a digo command is issued, an internal state variable within the firmware is set to â€˜1â€™, and it stays in that state until the algorithm has completed. Upon completion, the state is set to â€˜0â€™. The â€˜pidsâ€™ command simply returns the value of the internal variable to determine if the algorithms is currently busy, or if it has finished, thus allowing subsequent digo commands to be issued w/o clobbering previous ones.
-//*/
-//        case 6 :
-//            r->Etat_Response=Rep_pids;
-//            pch = strtok (str," ");
-//            r->Read_Pids = sscanf(response, "%d", &i);
-//            break;
+            /*Once a digo command is issued, an internal state variable within the firmware is set to â€˜1â€™, and it stays in that state until the algorithm has completed. Upon completion, the state is set to â€˜0â€™. The â€˜pidsâ€™ command simply returns the value of the internal variable to determine if the algorithms is currently busy, or if it has finished, thus allowing subsequent digo commands to be issued w/o clobbering previous ones.
+*/
+        case 6 :
+            r->Etat_Response=Rep_pids;
+            pch = strtok (str," ");
+            r->Read_Pids = sscanf(response, "%d", &i);
+            break;
 
-//            /*The vel command returns the left and right wheel velocities. The
-//velocity returned is based on the PIDL parameter configuration.*/
-//        case 7 :
-//            r->Etat_Response=Rep_vel;
-//            pch = strtok (str," ");
-//            r->Read_Vitesse_mot1 = pch;
-//            r->Read_Vitesse_mot2 = pch;
-//            break;
+            /*The vel command returns the left and right wheel velocities. The
+velocity returned is based on the PIDL parameter configuration.*/
+        case 7 :
+            r->Etat_Response=Rep_vel;
+            pch = strtok (str," ");
+            r->Read_Vitesse_mot1 = pch;
+            r->Read_Vitesse_mot2 = pch;
+            break;
 
-//    }
-//return 0;
-//}
+    }
+    return 0;
+}
