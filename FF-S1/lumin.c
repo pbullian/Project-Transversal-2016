2d3306b6f2303e970f0fe6d941bcabab84cb1fb4
pbullian <Pablo@Pauls-Warmachine.local>
2016-04-28 09:13:52 +0200

    added lumiere command

---
+++ FF-S1/lumin.c
@@ -0,0 +1,74 @@
+//------------------------------------------------------------------------------------
+// serializer.c
+//------------------------------------------------------------------------------------
+// AUTH:PEREL/BULLIAN
+// DATE:15/03/2016
+//
+// Target: C8051F02x
+// Tool chain: KEIL Eval 'c'
+//
+//------------------------------------------------------------------------------------
+// Includes
+//------------------------------------------------------------------------------------
+#include <string.h>
+
+#include <stdlib.h>
+#include <intrins.h>
+#include "LIB_BSE_INT_UART.h"
+#include "serializer.h"
+#include <stdio.h>
+
+
+
+
+/*
+* intensity : intensité lumineuse exprimée en pourcentage de luminosité
+*• Lum_ON : Durée d’allumage exprimée en dixièmes de seconde (de 1 à 100)
+*• Lum_OFF: Durée d’extinction exprimée en dixièmes de seconde (de 1 à 100)
+*• Lum_Nbre: Nombre de cycles d’allumage-Extinction (de 1 à 100)
+*/
+
+void Lumiere (unsigned char* intensity, unsigned char* Lum_ON, unsigned char* Lum_OFF, unsigned char* Lum_Nbre) {
+
+    if(intensity != 0){
+        Send_Lumin("");
+        return;
+    }
+
+    if(Lum_ON != 0){
+        Send_Lumin("");
+        return;
+    }
+
+    if(Lum_OFF != 0){
+        Send_Lumin("");
+        return;
+    }
+
+    if(Lum_Nbre != 0){
+        Send_Lumin("");
+        return;
+    }
+
+
+}
+
+/*Extinction inconditionnelle de la source lumineuse*/
+void Lumiere_Stop (void) {
+
+
+
+}
+
+
+
+void Send_Lumin(const char* char_ptr){
+    char command[50];
+
+    strcpy(command, char_ptr);
+
+    //Send_String(command);
+    serOutstring(command);
+}
+
+
